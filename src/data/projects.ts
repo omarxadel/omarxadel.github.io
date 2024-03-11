cec199747be17ea48681facfae41e43ee62ab75c
Omar Adel <omarxadel21@gmail.com>
2024-03-11 16:00:49 +0200

    feat: added latest project `phonebook-app`

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,6 +7,20 @@ export type Project = {
 
 const projects: Project[] = [
   {
+    title: "Phonebook Lite",
+    techs: [
+      "TypeScript",
+      "ExpressJS",
+      "Prisma",
+      "PostgreSQL",
+      "Supabase",
+      "NextJS",
+      "Tailwind",
+      "Full-Stack Development",
+    ],
+    link: "https://github.com/omarxadel/phonebook-app",
+  },
+  {
     title: "Hearsome",
     techs: ["React JS", "Flutter", "Express JS", "Deep Learning (ASR)"],
     link: "https://github.com/lectly",
